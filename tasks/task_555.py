e56d2978ce48c7d25cb0036092c80bcc23e674d7
Ostap Halapats <ostapgalapac@gmail.com>
2018-03-12 20:29:42 +0200

    Fixed docstrings according to reST

--- tasks/task_555.py
+++ tasks/task_555.py
@@ -1,17 +1,16 @@
 """"This module solves the task 555."""
 
 
-def current_row(number):
-    """"
+def current_row(number: int) -> list:
+    """
     Take a natural number and return current row of Pascal's triangle,
     where 'number' is its length.
 
-    Args:
-        number (int): any natural number
+    :param int number: Any natural number.
+    :return list row: Represents current row of Pascal's triangle.
 
-    Returns:
-        row (list): represents current row of Pascal's triangle
-
+    >>> current_row(10)
+    [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
     """
     row = []
     for i in range(number):
@@ -22,8 +21,7 @@ def current_row(number):
             row.append(c_row[i - 1] + c_row[i])
     return row
 
-
-def triagle(number_of_rows):
+def triagle(number_of_rows: int) -> list:
     """"
     Take a natural number and make Pascal's triangle list,
     where each sublist is a row of Pascal's triangle.
@@ -34,6 +32,8 @@ def triagle(number_of_rows):
     Returns:
         result (list): represents Pascal's triangle
 
+    >>> triagle(5)
+    [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
     """
     result = []
     for i in range(number_of_rows):
